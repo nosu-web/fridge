444c1d1f50a9572b67c3d0f0fefa6f1421ec8c34
NOSU-WEB <79894331+nosu-web@users.noreply.github.com>
2021-05-12 09:48:00 +0300

    DB dump

---
+++ _sql/p918827r_fridge.sql
@@ -0,0 +1,247 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.7
+-- https://www.phpmyadmin.net/
+--
+-- Хост: localhost
+-- Время создания: Май 12 2021 г., 09:43
+-- Версия сервера: 5.7.21-20-beget-5.7.21-20-1-log
+-- Версия PHP: 5.6.40
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `p918827r_fridge`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `fridge`
+--
+-- Создание: Апр 28 2021 г., 07:08
+--
+
+DROP TABLE IF EXISTS `fridge`;
+CREATE TABLE `fridge` (
+  `id` int(11) NOT NULL,
+  `products_id` int(11) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `users_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `measure`
+--
+-- Создание: Апр 28 2021 г., 07:15
+--
+
+DROP TABLE IF EXISTS `measure`;
+CREATE TABLE `measure` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `shortname` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `measure`
+--
+
+INSERT INTO `measure` (`id`, `name`, `shortname`) VALUES
+(1, 'литры', 'л.'),
+(2, 'миллиграмм', 'мг.'),
+(3, 'килограмм', 'кг.'),
+(4, 'грамм', 'г.'),
+(5, 'миллилитр', 'мл.'),
+(6, 'штуки', 'шт.');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `products`
+--
+-- Создание: Апр 28 2021 г., 07:01
+--
+
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `measure_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `recipes`
+--
+-- Создание: Апр 28 2021 г., 06:53
+--
+
+DROP TABLE IF EXISTS `recipes`;
+CREATE TABLE `recipes` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `recipes_products`
+--
+-- Создание: Апр 28 2021 г., 07:03
+--
+
+DROP TABLE IF EXISTS `recipes_products`;
+CREATE TABLE `recipes_products` (
+  `id` int(11) NOT NULL,
+  `recipes_id` int(11) NOT NULL,
+  `products_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+-- Создание: Апр 28 2021 г., 06:51
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `registered` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `registered`) VALUES
+(1, 'admin', 'admin@mail.ru', '$2y$10$VRox.wT4rFmDg83ExcS3FOQi/qXm62rCNzioH6XKoemdpENhdrcqK', '2021-05-11 00:08:22');
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `fridge`
+--
+ALTER TABLE `fridge`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `products_id` (`products_id`),
+  ADD KEY `users_id` (`users_id`);
+
+--
+-- Индексы таблицы `measure`
+--
+ALTER TABLE `measure`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `measure_id` (`measure_id`);
+
+--
+-- Индексы таблицы `recipes`
+--
+ALTER TABLE `recipes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `recipes_products`
+--
+ALTER TABLE `recipes_products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `products_id` (`products_id`),
+  ADD KEY `recipes_id` (`recipes_id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `fridge`
+--
+ALTER TABLE `fridge`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `measure`
+--
+ALTER TABLE `measure`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT для таблицы `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `recipes`
+--
+ALTER TABLE `recipes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `recipes_products`
+--
+ALTER TABLE `recipes_products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `fridge`
+--
+ALTER TABLE `fridge`
+  ADD CONSTRAINT `fridge_ibfk_1` FOREIGN KEY (`products_id`) REFERENCES `products` (`id`),
+  ADD CONSTRAINT `fridge_ibfk_2` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`);
+
+--
+-- Ограничения внешнего ключа таблицы `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`measure_id`) REFERENCES `measure` (`id`);
+
+--
+-- Ограничения внешнего ключа таблицы `recipes_products`
+--
+ALTER TABLE `recipes_products`
+  ADD CONSTRAINT `recipes_products_ibfk_1` FOREIGN KEY (`products_id`) REFERENCES `products` (`id`),
+  ADD CONSTRAINT `recipes_products_ibfk_2` FOREIGN KEY (`recipes_id`) REFERENCES `recipes` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
