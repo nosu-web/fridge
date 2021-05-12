bd30ed938f96f750f4cba852a8eff1814cf9281b
goudiev <t.goudiev@yandex.ru>
2021-05-12 08:30:58 +0300

    v2.0

--- add.php
+++ add.php
@@ -4,35 +4,33 @@ require("config/common.php");
 
 /* Подключение к БД */
 require("includes/mysqli.inc.php");
+
+/* Проверка авторизации */
+require("includes/auth.inc.php");
 ?>
 <!doctype html>
 <html lang="ru">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Team Cook">
-    <meta name="keywords" content="холодильник, рецепты">
-
-    <title>Fridge</title>
-
+    
     <!-- CSS -->
     <?php require("templates/styles.tpl.php"); ?>
+    <link rel="stylesheet" href="vendor/leaflet-1.7.1/dist/leaflet.css">
+
+    <title>aMap</title>
   </head>
   <body>
+    <!-- HEADER -->
     <?php require("templates/header.tpl.php"); ?>
-    
-  <div class="container-fluid">
-    <div class="row">
-      <?php require("templates/sidebar.tpl.php"); ?>
-
-      <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 pt-3 pb-2 mb-3 ">
-        <?php require("templates/index.tpl.php"); ?>
-      </main>
-    </div>
-  </div>
-
-  <?php require("templates/scripts.tpl.php"); ?>
+    <main class="container-fluid">
+      <!-- PAGE -->
+      <?php require("templates/add.tpl.php"); ?>
+    </main>
+    <!-- JAVASCRIPT -->
+    <?php require("templates/scripts.tpl.php"); ?>
+    <script src="vendor/leaflet-1.7.1/dist/leaflet.js"></script>
+  
   </body>
 </html>
 <?php
